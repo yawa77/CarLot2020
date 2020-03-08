b89bd7eeb24fa068a38d4e4d866615e4f46645f3
yawa77 <yawa77@users.noreply.github.com>
2020-03-08 15:06:45 -0500

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLot2020
+{
+    class Vehicle
+    {
+        static int vehCount;
+        string make;
+        string model;
+        int purchasePrice;
+        int askingPrice;
+
+
+        public Vehicle(string vehMake, string vehModel, int purchasePrice, int askingPrice)
+        {
+            vehMake = make;
+            vehModel = model;
+            this.purchasePrice = purchasePrice;
+            this.askingPrice = askingPrice;
+        }
+
+
+    }
+}
